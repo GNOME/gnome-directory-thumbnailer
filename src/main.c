bc5af749d5ee551af46e2a41280eacce3601c12a
Philip Withnall <philip.withnall@collabora.co.uk>
2013-10-11 15:13:15 +0100

    build: Relicence to LGPLv2.1+
    Let’s be a little more permissive. I’m the only author so far, so no
    permission needed to be sought from other contributors.

--- src/main.c
+++ src/main.c
@@ -3,18 +3,18 @@
  * gnome-directory-thumbnailer
  * Copyright (C) 2013 Collabora Ltd.
  *
- * gnome-directory-thumbnailer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * gnome-directory-thumbnailer is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * gnome-directory-thumbnailer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with gnome-directory-thumbnailer.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with gnome-directory-thumbnailer.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authors:
  *  • Philip Withnall <philip.withnall@collabora.co.uk>
