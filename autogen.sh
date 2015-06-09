cd89454afd803bcde1e1527929e2d0f5caaabe7f
Philip Withnall <philip.withnall@collabora.co.uk>
2015-06-09 13:28:16 +0100

    build: Update autogen.sh to drop unnecessary use of gnome-autogen
    https://wiki.gnome.org/Projects/GnomeCommon/Migration

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME=gnome-directory-thumbnailer
-
 (test -f $srcdir/configure.ac) || {
-	echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level $PKG_NAME directory"
+	echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level project directory"
 	exit 1
 }
 
-which gnome-autogen.sh || {
-	echo "You need to install gnome-common from GNOME Git"
-	exit 1
-}
+PKG_NAME=`autoconf --trace 'AC_INIT:$1' "$srcdir/configure.ac"`
 
-REQUIRED_PKG_CONFIG_VERSION=0.17.1 REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh "$@"
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+	echo "**Warning**: I am going to run \`configure' with no arguments." >&2
+	echo "If you wish to pass any to it, please specify them on the" >&2
+	echo \`$0\'" command line." >&2
+	echo "" >&2
+fi
+
+set -x
+aclocal --install || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+set +x
+
+if [ "$NOCONFIGURE" = "" ]; then
+	set -x
+	$srcdir/configure "$@" || exit 1
+	set +x
+
+	if [ "$1" = "--help" ]; then exit 0 else
+		echo "Now type \`make\' to compile $PKG_NAME" || exit 1
+	fi
+else
+	echo "Skipping configure process."
+fi
